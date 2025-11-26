0168c6d4c66c6fbbdaadf381a43b410f3207e891
Colbren Fujimoto <125836155+colbren@users.noreply.github.com>
2025-11-26 11:28:55 -1000

    Fixing ESLint errors

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -6,18 +6,16 @@ import 'leaflet/dist/leaflet.css';
 
 const center: LatLngExpression = [21.2995, -157.8170];
 
-const LeafletMap = () => {
-  return (
-    <div style={{ height: '300px', width: '100%' }}>
-      <MapContainer
-        center={center}
-        zoom={16}
-        style={{ height: '100%', width: '100%' }}
-      >
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      </MapContainer>
-    </div>
-  );
-};
+const LeafletMap = () => (
+  <div style={{ height: '300px', width: '100%' }}>
+    <MapContainer
+      center={center}
+      zoom={16}
+      style={{ height: '100%', width: '100%' }}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+    </MapContainer>
+  </div>
+);
 
 export default LeafletMap;
