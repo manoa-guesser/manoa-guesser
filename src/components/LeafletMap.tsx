61e7bab0121e68fc8689aaef20ca06888ab0d12c
Colbren Fujimoto <125836155+colbren@users.noreply.github.com>
2025-11-26 11:07:49 -1000

    Vercel fix

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -1,18 +1,23 @@
 'use client';
 
 import { MapContainer, TileLayer } from 'react-leaflet';
+import type { LatLngExpression } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-const LeafletMap = () => (
-  <div style={{ height: '300px', width: '100%' }}>
-    <MapContainer
-      center={[21.2995, -157.8170]} // UH Mānoa
-      zoom={16}
-      style={{ height: '100%', width: '100%' }}
-    >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-    </MapContainer>
-  </div>
-);
+const center: LatLngExpression = [21.2995, -157.8170];
+
+const LeafletMap = () => {
+  return (
+    <div style={{ height: '300px', width: '100%' }}>
+      <MapContainer
+        center={center}
+        zoom={16}
+        style={{ height: '100%', width: '100%' }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </MapContainer>
+    </div>
+  );
+};
 
 export default LeafletMap;
