6f44d9e8036e6cb463295ed8086bcf8e9561a761
Joshua Chow <98932409+ZhouWeiJosh@users.noreply.github.com>
2025-12-01 10:10:47 -1000

    Created plawright test

---
+++ tests/login.spec.ts
@@ -0,0 +1,18 @@
+import { test } from '@playwright/test';
+
+test.use({
+  storageState: 'john-auth.json'
+});
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('link', { name: 'Sign in' }).click();
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await page.getByRole('link', { name: 'Sign up' }).click();
+  await page.locator('input[name="email"]').click();
+  await page.locator('input[name="email"]').fill('admin@foo.com');
+  await page.getByRole('button', { name: 'Reset' }).click();
+  await page.getByRole('button', { name: 'Register' }).click();
+  await page.getByRole('link', { name: 'Sign in' }).click();
+});
